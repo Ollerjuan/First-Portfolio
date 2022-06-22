8e5e80fc361d28ecfb96a6c73805c192c7a0d647
Juan <juan_oller@yahoo.com>
2022-06-22 13:01:31 -0500

    added nav bar

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.scss';
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import Layout from './components/Layout';
+
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+        <Route path ="/" element={<Layout />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App;
---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,18 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={'https://i.imgur.com/9YejLKh.png'} alt='logo'/>
+        
+        </Link>
+        <nav>
+            
+        </nav>
+    </div>
+)
+  
+
+
+export default Sidebar
