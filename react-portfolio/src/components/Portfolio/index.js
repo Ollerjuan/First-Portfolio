11008a7377dc7801c9c710257594711e6351d14b
Juan <juan_oller@yahoo.com>
2022-06-28 12:41:12 -0500

    adding styles to portfolio page

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -1,13 +1,43 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import {useState} from 'react'
-// import portfolioData from '../../data/portfolio.json';
-import portfolio from '../../data/portfolio.json'
+import portfolioData from '../../data/portfolio.json';
+// import portfolio from '../../data/portfolio.json'
 
 
 const Portfolio = () => {
-    const [letterClass, setLetterClass] = useState('text-animate');
-    console.log(portfolio)
+    const [letterClass] = useState('text-animate');
+    console.log(portfolioData)
+
+     const renderPortfolio = (portfolio) => {
+        return (
+            <div className='images-container'>
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            
+                        <div className='image-box' key={idx}>
+                            <img classname="portfolio-image" src={port.cover} alt='protfolio' />
+                        <>
+                        <div className='content'>
+                            <p classNam='title'>
+                                {port.title}
+                            </p>
+                            <button className='button'
+                                onClick={() => window.open(port.url)}>
+                                    View
+                            </button>
+                        </div>
+                        </>
+                        </div>
+                        
+                        )
+                    })
+                }
+            
+            </div>
+        )
+     }       
 
     return (
         <div className='container portfolio-page'>
@@ -32,7 +62,8 @@ const Portfolio = () => {
                     </ul>
 
                  </div> 
-              
+{/* allowing the images to show up on the portfolio page  */}
+              <div>{renderPortfolio(portfolioData.portfolio)}</div>
     </div>
     )
 }
